cc7c634ec32cc18f6dacf6b5a54222c9630a06d3
ken1286 <kindle1286@gmail.com>
2019-10-07 16:14:38 -0700

    step 1 of count_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -10,17 +10,17 @@ def selection_sort(arr):
             if arr[n] < arr[smallest_index]:
                 smallest_index = n
 
-        # TO-DO: swap
         arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+        # TO-DO: swap
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     arrayLen = len(arr) - 1
-    for n in range(0, arrayLen):  # loop through and compare
+    for n in range(0, arrayLen):
         if arr[n] > arr[n+1]:
-            arr[n], arr[n+1] = arr[n+1], arr[n]
+            arr[n+1], arr[n] = arr[n], arr[n+1]
         for i in range(0, arrayLen - n):  # loop through again and ignore end
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
@@ -28,6 +28,14 @@ def bubble_sort(arr):
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort(arr, maximum=-1):
+arr = [3, 3, 3, 1, 1, 5]
+count = [0] * len(arr)
 
+
+def count_sort(arr, maximum=-1):
+    # Count the number of elements with distinct values
+    count_arr = [0] * len(arr)
+    for n in arr:
+        count_arr[n] += 1
+    print(count_arr)
     return arr
